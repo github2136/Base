e629daad31428f78b0524fe3a1d8b98f9040d08d
github2136 <github2136@163.com>
2020-11-09 17:32:54 +0800

    log文件日志保存

--- basemvvm/src/main/java/com/github2136/basemvvm/BaseApplication.kt
+++ basemvvm/src/main/java/com/github2136/basemvvm/BaseApplication.kt
@@ -1,12 +1,12 @@
 package com.github2136.basemvvm
 
 import android.app.Application
+import android.os.HandlerThread
 import androidx.appcompat.app.AppCompatActivity
 import com.github2136.util.CrashHandler
-import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.DiskLogAdapter
-import com.orhanobut.logger.Logger
-import com.orhanobut.logger.PrettyFormatStrategy
+import com.github2136.util.FileUtil
+import com.orhanobut.logger.*
+import java.io.File
 import java.util.*
 
 /**
@@ -20,23 +20,10 @@ open class BaseApplication : Application() {
 
 //        logcat打印
         val prettyFormatStrategy = PrettyFormatStrategy
-                .newBuilder()
-                .tag("MVVM-TAG")
+            .newBuilder()
+            .tag("MVVM-TAG")
         Logger.addLogAdapter(AndroidLogAdapter(prettyFormatStrategy.build()))
 
-        //log日志本地保存
-//        val MAX_BYTES = 500 * 1024
-//        val folder = FileUtil.getExternalStorageProjectPath(this) + File.separator + "Log"
-//        val ht = HandlerThread("AndroidFileLogger.$folder")
-//        ht.start()
-//        val handler = DiskLog.WriteHandler(ht.looper, folder, MAX_BYTES)
-//        val logStrategy = DiskLog(handler)
-//
-//        val prettyFormatStrategy = CsvFormatStrategy
-//            .newBuilder()
-//            .logStrategy(logStrategy)
-//            .tag("NATURAL_RESERVE")
-//        Logger.addLogAdapter(DiskLogAdapter(prettyFormatStrategy.build()))
         CrashHandler.getInstance(this, BuildConfig.DEBUG).setCallback(object : CrashHandler.CrashHandlerCallback {
             override fun finishAll() {
                 this@BaseApplication.finishAll()
@@ -48,6 +35,24 @@ open class BaseApplication : Application() {
         })
     }
 
+    /**
+     * 日志文件保存
+     */
+    fun setFileLog() {
+        //log日志本地保存
+        val MAX_BYTES = 500 * 1024
+        val folder = FileUtil.getExternalStorageProjectPath(this) + File.separator + "Log"
+        val ht = HandlerThread("AndroidFileLogger.$folder")
+        ht.start()
+        val handler = DiskLog.WriteHandler(ht.looper, folder, MAX_BYTES)
+        val logStrategy = DiskLog(handler)
+
+        val fileFormatStrategy = CsvFormatStrategy
+            .newBuilder()
+            .logStrategy(logStrategy)
+            .tag("FILE_LOG")
+        Logger.addLogAdapter(DiskLogAdapter(fileFormatStrategy.build()))
+    }
 
     fun addActivity(act: AppCompatActivity) {
         this.mActivitys.add(act)
