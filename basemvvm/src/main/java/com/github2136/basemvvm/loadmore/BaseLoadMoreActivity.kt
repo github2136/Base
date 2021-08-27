62124dbec41ef056a3c68d186f48a48be38edadf
github2136 <github2136@163.com>
2021-08-27 12:36:57 +0800

    加载更多增加GridLayoutManager支持

--- basemvvm/src/main/java/com/github2136/basemvvm/loadmore/BaseLoadMoreActivity.kt
+++ basemvvm/src/main/java/com/github2136/basemvvm/loadmore/BaseLoadMoreActivity.kt
@@ -3,6 +3,7 @@ package com.github2136.basemvvm.loadmore
 import android.os.Bundle
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github2136.basemvvm.BaseActivity
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -15,7 +16,22 @@ abstract class BaseLoadMoreActivity<V : BaseLoadMoreVM<T>, B : ViewDataBinding, 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         findViewById<SwipeRefreshLayout>(R.id.srlList).setColorSchemeResources(R.color.colorPrimary)
-        findViewById<RecyclerView>(R.id.rvList).addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        findViewById<RecyclerView>(R.id.rvList).let {
+            val lm = it.layoutManager
+            if (lm is GridLayoutManager) {
+                lm.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return if (position == it.adapter!!.itemCount - 1) {
+                            lm.spanCount
+                        } else {
+                            1
+                        }
+                    }
+                }
+            } else {
+                it.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+            }
+        }
         vm.baseInitData()
     }
 
--- basemvvm/src/main/java/com/github2136/basemvvm/loadmore/BaseLoadMoreFragment.kt
+++ basemvvm/src/main/java/com/github2136/basemvvm/loadmore/BaseLoadMoreFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.github2136.basemvvm.BaseFragment
@@ -18,7 +19,22 @@ abstract class BaseLoadMoreFragment<V : BaseLoadMoreVM<T>, B : ViewDataBinding, 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bind.root.findViewById<SwipeRefreshLayout>(R.id.srlList)?.setColorSchemeResources(R.color.colorPrimary)
-        bind.root.findViewById<RecyclerView>(R.id.rvList).addItemDecoration(DividerItemDecoration(mContext, DividerItemDecoration.VERTICAL))
+        bind.root.findViewById<RecyclerView>(R.id.rvList).let {
+            val lm = it.layoutManager
+            if (lm is GridLayoutManager) {
+                lm.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return if (position == it.adapter!!.itemCount - 1) {
+                            lm.spanCount
+                        } else {
+                            1
+                        }
+                    }
+                }
+            } else {
+                it.addItemDecoration(DividerItemDecoration(mContext, DividerItemDecoration.VERTICAL))
+            }
+        }
         if (autoInit) {
             vm.baseInitData()
         }
