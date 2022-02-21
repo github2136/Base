6ef49efec9b375a65b62cc88c08594a62cf5a726
github2136 <github2136@163.com>
2022-02-21 15:41:47 +0800

    修改

--- basemvvm/src/main/java/com/github2136/basemvvm/BaseVM.kt
+++ basemvvm/src/main/java/com/github2136/basemvvm/BaseVM.kt
@@ -3,21 +3,20 @@ package com.github2136.basemvvm
 import android.app.Application
 import android.os.Handler
 import android.os.Looper
-import android.util.SparseArray
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.github2136.util.JsonUtil
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
-import java.util.*
 
 /**
  * Created by YB on 2019/8/29
  */
 abstract class BaseVM(app: Application) : AndroidViewModel(app) {
     protected val TAG = this.javaClass.name
+    val mJsonUtil by lazy { JsonUtil.instance }
+
     // protected val jobs = mutableMapOf<UUID, Job>()
     val loadingStr = "请稍后……"
 
